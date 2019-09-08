3435091dce9e3985a478efb3abe9d69fdc75d15d
shchuko <yaroshchuk2000@gmail.com>
2019-09-08 14:46:38 +0300

    NumericSet implemention finished. Testing required

--- OOP_task1/OOP_task1/ItmoNamespace/NumericSet.cs
+++ OOP_task1/OOP_task1/ItmoNamespace/NumericSet.cs
@@ -4,40 +4,156 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace ItmoNamespace
 {
-    public class NumericSet<T>
+    public class NumericSet<T> where T : INumber
     {
-        public void Add(T element)
+        public bool IsEmpty()
         {
-            if (size == capacity)
-            {
-                IncreaseCapacity();
-            }
-
-            data[size++] = element;
+            return size == 0;
         }
 
+        public bool Contains(T element)
+        {
+            return Find(element) != null;
+        }
 
+        public void Add(T element)
+        {
+            if (Find(element) != null)
+            {
+                return;
+            }
 
-        private static int DEFAULT_CAPACITY = 10;
+            Node temp = Emplace(element);
+            UpdateMinMax(temp);
+            ++size;
+        }
+
+        public int HowManyIsLesserThan(T element)
+        {
+            if (lesserRequestResultCache != -1 && lesserRequestElementCache.Equals(element))
+            {
+                return lesserRequestResultCache;
+            }
+
+            int lesserElementsCounter = 0;
+            Node temp = dataHead;
+            while (temp != null && element.IsGreaterThan(temp.data))
+            {
+                temp = temp.next;
+                ++lesserElementsCounter;
+            }
+
+            lesserRequestElementCache = element;
+            lesserRequestResultCache = lesserElementsCounter;
+            return lesserElementsCounter;
+        }
+        public int HowManyIsGreaterThan(T element)
+        {
+            if (greaterRequestResultCache != -1 && greaterRequestElementCache.Equals(element))
+            {
+                return greaterRequestResultCache;
+            }
+
+            int lesserOrEqualElementsCounter = 0;
+            Node temp = dataHead;
+            while (temp != null && element.IsGreaterThan(temp.data))
+            {
+                temp = temp.next;
+                ++lesserOrEqualElementsCounter;
+            }
+
+            int greaterElementsCounter = size - lesserOrEqualElementsCounter;
+            if (temp != null && temp.data.Equals(element))
+            {
+                --greaterElementsCounter
+            }
+
+            lesserRequestElementCache = element;
+            lesserRequestResultCache = lesserOrEqualElementsCounter;
+            return lesserOrEqualElementsCounter;
+        }
 
         private int size = 0;
-        private int capacity = DEFAULT_CAPACITY;
-        private T[] data = new T[DEFAULT_CAPACITY];
-        private T minValCache;
-        private T maxValCache;
+        private Node dataHead = null;
+        
+        private Node minValCache = null;
+        private Node maxValCache = null;
 
-        private void IncreaseCapacity()
+        private T lesserRequestElementCache;
+        private int lesserRequestResultCache = -1;
+
+        private T greaterRequestElementCache;
+        private int greaterRequestResultCache = -1;
+
+        private Node Find(T element)
         {
-            T[] temp = new T[capacity * 2];
-            capacity *= 2;
-
-            for (int i = 0; i < size; ++i)
-            {
-                temp[i] = data[i];
+            Node temp = dataHead;
+            while (temp != null && !temp.data.Equals(element)) {
+                temp = temp.next;
             }
-            data = temp;
+            return temp;
+        }
+
+        private Node Emplace(T element)
+        {
+            if (dataHead == null)
+            {
+                dataHead = new Node(element);
+                return dataHead;
+            }
+
+            Node nodeToCompare = dataHead;
+            Node nodeToComparePrev = null;
+
+            while (nodeToCompare.next != null && element.IsGreaterThan(nodeToCompare.data))
+            {
+                nodeToComparePrev = nodeToCompare;
+                nodeToCompare = nodeToCompare.next;
+            }
+
+            Node insertedElementNode = new Node(element, nodeToCompare);
+            if (nodeToComparePrev == null)
+            {
+                dataHead = insertedElementNode;
+            }
+            else
+            {
+                nodeToComparePrev.next = insertedElementNode;
+            }
+            return insertedElementNode;
+        }
+
+        private void UpdateMinMax(Node element)
+        {
+            if (maxValCache == null || element.data.IsGreaterThan(maxValCache.data))
+            {
+                maxValCache = element;
+            }
+
+            if (minValCache == null || !element.data.IsGreaterThan(minValCache.data))
+            {
+                minValCache = element;
+            }
+        }
+
+        private class Node
+        {
+            public T data;
+            public Node next = null;
+
+            public Node(T data, Node next)
+            {
+                this.data = data;
+                this.next = next;
+            }
+
+            public Node(T data)
+            {
+                this.data = data;
+            }
         }
 
     }
