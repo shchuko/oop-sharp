744fad7973255e4fff6753878e5ce136f23d385e
shchuko <yaroshchuk2000@gmail.com>
2019-09-08 16:32:22 +0300

    First test

--- OOP_task1/OOP_task1/Program.cs
+++ OOP_task1/OOP_task1/Program.cs
@@ -12,7 +12,29 @@ namespace OOP_task1
     {
         static void Main(string[] args)
         {
-            Console.Out.WriteLine(new Rational(3, 5));
+            NumericSet<Rational> numericSet = new NumericSet<Rational>();
+            numericSet.Add(new Rational(1, 8));
+            numericSet.Add(new Rational(1, 4));
+            numericSet.Add(new Rational(1, 2));
+            // place for 3/5
+            numericSet.Add(new Rational(3, 4));
+            numericSet.Add(new Rational(1, 4));     // won't be added, size == 4
+
+            foreach (Rational rat in numericSet)
+            {
+                Console.Write(rat + " ");
+            }
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Size: " + numericSet.Size());
+            Console.WriteLine("Max: " + numericSet.GetMax());
+            Console.WriteLine("Min: " + numericSet.GetMin());
+
+            Console.WriteLine("Greater than 3/5: " + numericSet.HowManyIsGreaterThan(new Rational(3, 5)));
+            Console.WriteLine("Lesser than 3/5: " + numericSet.HowManyIsLesserThan(new Rational(3, 5)));
+
+            Console.WriteLine("Greater than 1/2: " + numericSet.HowManyIsGreaterThan(new Rational(1, 2)));
+            Console.WriteLine("Lesser than 1/2: " + numericSet.HowManyIsLesserThan(new Rational(1, 2)));
             Console.Read();
         }
     }
