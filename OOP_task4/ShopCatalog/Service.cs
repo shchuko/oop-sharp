c6da5b619f58fcdc2b7ba921934484dac5556a68
Vladislav Yaroshchuk <yaroshchuk2000@gmail.com>
2019-10-10 13:40:16 +0300

    Create ExecCreateShop() method in Service

--- OOP_task4/ShopCatalog/Service.cs
+++ OOP_task4/ShopCatalog/Service.cs
@@ -66,5 +66,29 @@ namespace ShopCatalog
             return result.ToArray();
         }
         
+        private string[] ExecCreateShop(string args)
+        {
+            Regex regex = new Regex(@".*shop-id=(\d+?);\s*shop-name='(.+?)';\s*shop-address='(.+?)'\s*");
+            if (!regex.IsMatch(args))
+            {
+                return new []{"Incorrect shop data"};
+            }
+
+            var matcher = regex.Match(args);
+            int shopId = int.Parse(matcher.Groups[1].Value);
+            string shopName = matcher.Groups[2].Value;
+            string shopAddress = matcher.Groups[3].Value;
+            
+            try
+            {
+                _dao.CreateShop(shopId, shopName, shopAddress);
+            }
+            catch (MissingDataConsistencyException e)
+            {
+                return new[] {"Creation error: shop already exists"};
+            }
+
+            return new[] {"Creation successful"};
+        }
     }
 }
